62934031167598b4a9374e6499273b41b54533ca
BZheng2963 <xml2963x@gmail.com>
2021-01-15 10:12:20 -0600

    Finished Chapters 8 and 9 of Flask Mega-Tutorial

--- microblog/config.py
+++ microblog/config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['whzheng@stu.naperville203.org']
+    POSTS_PER_PAGE = 25
